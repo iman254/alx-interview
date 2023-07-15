6803d4b69b5f930e68b853c54b306bbaa5212de8
iman <stephanieiman254@gmail.com>
2023-07-15 17:15:03 +0300

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#/usr/bin/python3
+"""A method that determines if all the boxes can be opened."""
+def canUnlockAll(boxes):
+    """this method creates a system that can keep track of the boxes open and groups them into a stack whiich is later popped till empty"""
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    stack = [0]
+
+    while stack:
+        box = stack.pop()
+        for key in boxes[box]:
+            if key < n and not visited[key]:
+                visited[key] = True
+                stack.append(key)
+
+    return all(visited)
